de353bfaf57cf7c02859d8b9132dfa176011fd8f
EKOUE-BLA Sandjison <ekoueblasandjison@gmail.com>
2024-05-16 11:17:53 +0000

    MIse en place de l'interface d'inscription

--- pages/articles.php
+++ pages/articles.php
@@ -2,20 +2,18 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="./ASSETS/CSS/style.css">
+    <link rel="stylesheet" href="../ASSETS/CSS/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-
+    <title>Articles</title>
 </head>
 
 <body>
-
     <?php
 
-    require_once './includes/menu.php'
+    require_once '../includes/menu.php';
     ?>
-
+    <h1>Articles</h1>
 </body>
 
 </html>
